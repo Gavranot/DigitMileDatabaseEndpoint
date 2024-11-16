7b9d6308282baba0b24dd6e93e9c77f86efe7ae9
Damjan Avramovski <damjan.avramovski@gmail.com>
2024-11-16 15:42:12 +0100

    feature: implement inserting and reading user data, implement inserting leve statistics for user
    Signed-off-by: Damjan Avramovski <damjan.avramovski@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import os
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import psycopg2
-from psycopg2 import sql
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -27,7 +25,86 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/api/insertdata', methods=['POST'])
+def checkIfUserExists(username, email):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # Assuming you have a table called 'your_table' with columns 'column1' and 'column2'
+    select_query = f"SELECT count(*) from digitmile_users where username = '{username}' and email = '{email}'"
+    cur.execute(select_query)
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    if result[0] > 0:
+        return True
+    return False
+
+def checkIfPasswordValid(username, email, password):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # Assuming you have a table called 'your_table' with columns 'column1' and 'column2'
+    select_query = f"SELECT count(*) from digitmile_users where username = '{username}' and email = '{email}' and user_password = '{password}'"
+    cur.execute(select_query)
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    if result[0] > 0:
+        return True
+    return False
+
+@app.route('/api/checkUserLogin', methods=['POST'])
+@cross_origin()
+def checkUserLogin():
+    if not request.is_json:
+        return jsonify({"error": "Invalid input: Expected JSON"}), 400
+
+    # Get the JSON data sent by Unity
+    data = request.get_json()
+
+    check = checkIfUserExists(data['user'], data['email'])
+    passwordValid = checkIfPasswordValid(data['user'], data['email'], data['password'])
+
+    if check and passwordValid:
+        return jsonify({"message": "User login verified successfully"}), 201
+    else:
+        return jsonify({"message": "User login verification failed"}), 401
+
+@app.route('/api/registerUser', methods=['POST'])
+@cross_origin()
+def registerUser():
+    if not request.is_json:
+        return jsonify({"error": "Invalid input: Expected JSON"}), 400
+
+    # Get the JSON data sent by Unity
+    data = request.get_json()
+    print(data)
+    username = data['user']
+    email = data['email']
+    password = data['password']
+
+    check = checkIfUserExists(username, email)
+
+    if check:
+        return jsonify({"message": "User already exists"}), 401
+    else:
+        insert_query = "INSERT INTO digitmile_users (username, email, user_password) VALUES (%s, %s, %s)"
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # Assuming you have a table called 'your_table' with columns 'column1' and 'column2'
+        cur.execute(insert_query, (username,email,password))
+
+        # Commit the transaction
+        conn.commit()
+
+        # Close the connection
+        cur.close()
+        conn.close()
+        return jsonify({"message": "User successfully registered"}), 201
+
+
+@app.route('/api/insertLevelStatistics', methods=['POST'])
 @cross_origin()
 def insert_data():
     try:
@@ -35,22 +112,20 @@ def insert_data():
         if not request.is_json:
             return jsonify({"error": "Invalid input: Expected JSON"}), 400
 
-        # Get the JSON data sent by Unity
-        data = request.get_json()
-
-        # Example data extraction (adjust fields as per your use case)
-        test = data.get('test')
-
-        if not test:
-            return jsonify({"error": "Missing data in the request"}), 400
-
-        # Insert data into the database
         conn = get_db_connection()
         cur = conn.cursor()
 
-        # Assuming you have a table called 'your_table' with columns 'column1' and 'column2'
-        insert_query = "INSERT INTO test (test) VALUES (%s)"
-        cur.execute(insert_query, (test,))
+        # Get the JSON data sent by Unity
+        data = request.get_json()
+        print(data)
+        levelstatistics = data['levelStatistics']
+
+
+        select_query = f"SELECT id FROM digitmile_users WHERE username = '{data['user']}';"
+        cur.execute(select_query)
+        user_id = cur.fetchone()
+        insert_query = f"INSERT INTO userlevelstatistics (level_user, level_number, score, place, correctMoves, wrongMoves, timeElapsed) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+        cur.execute(insert_query, (user_id, levelstatistics.get('level'), levelstatistics.get('score'), levelstatistics.get('place'), levelstatistics.get('correctMoves'), levelstatistics.get('wrongMoves'), levelstatistics.get('timeElapsed')))
 
         # Commit the transaction
         conn.commit()
@@ -63,13 +138,10 @@ def insert_data():
         return jsonify({"message": "Data inserted successfully"}), 201
 
     except Exception as e:
+        print(f"Error: {e}")
         return jsonify({"error": str(e)}), 500
 
-@app.route('/api/getdata', methods=['GET'])
-def get_data():
-    return jsonify({'Success': 'Get data reached!'}), 200
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
